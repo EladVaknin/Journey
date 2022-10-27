6fb35b1ede1f1894a961de41fdcd0bf5fb16119c
EladVaknin <74238558+EladVaknin@users.noreply.github.com>
2022-10-27 16:52:30 +0300

    add checks

--- app/src/main/java/com/example/myjourney/activities/TakeDetailsForJourneyActivity.java
+++ app/src/main/java/com/example/myjourney/activities/TakeDetailsForJourneyActivity.java
@@ -1,9 +1,12 @@
 package com.example.myjourney.activities;
 
+import android.content.SyncStatusObserver;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
@@ -43,26 +46,145 @@ public class TakeDetailsForJourneyActivity extends AppCompatActivity {
     }
 
     private void CalculatingTheMainAlgorithm() {
-        // the main algorithm build on groups by this details
-
         String target = mTargetText.getText().toString();
         String paceStatus = mPaceStatus.getText().toString();
-        String runningStatus = mRunningStatus.getText().toString();
+        String currentRunningStatus = mRunningStatus.getText().toString();
         String age = CacheUtilities.getAge(this);
 
 
-        // checks if we have valid details
+        // checks if we got valid details
+        int minOpt = 5;
+        int midOpt = 10;
+        double maxOpt = 21.2;
+        int intTmpTarget = Integer.parseInt(target);
+        double doubleTmpTarget = Double.parseDouble(target);
+
+        if (intTmpTarget != minOpt || intTmpTarget != midOpt){
+            Toast.makeText(this,"Please chose one from the options",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (doubleTmpTarget != maxOpt|| TextUtils.isEmpty(target)){
+            Toast.makeText(this,"Please chose one from the options",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(paceStatus) || TextUtils.isEmpty(currentRunningStatus)){
+            Toast.makeText(this,"One of the fields is empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
 
 
+        // check the  range of current Running week range and status inputs.
+        int minRun =0;
+        int maxRun = 99;
+        double minPace = 00.00;
+        double maxPace = 99.55;
+        int tmpRunStatus = Integer.parseInt(currentRunningStatus);
+        double tmpPace = Double.parseDouble(paceStatus);
+
+        if (tmpRunStatus < minRun || tmpRunStatus >= maxRun){
+            Toast.makeText(this,"Please enter a valid running input",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (tmpPace <= minPace || tmpPace >= maxPace){
+            Toast.makeText(this,"Please enter a valid pace input",Toast.LENGTH_SHORT).show();
+            return;
+        }
+//        CheckValid();
+
+        // add the new details to firebase
+        mDBuser.child(mAuth.getCurrentUser().getUid()).child("target").setValue(target);
+        mDBuser.child(mAuth.getCurrentUser().getUid()).child("pace status").setValue(paceStatus);
+        mDBuser.child(mAuth.getCurrentUser().getUid()).child("current running status").setValue(currentRunningStatus);
 
 
-
-
-
-        // the groups
-
-
+        // the groups by age,pace status, current running status.
 
     }
 
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//    private void CheckValid() {
+//        String target = mTargetText.getText().toString();
+//        String paceStatus = mPaceStatus.getText().toString();
+//        String currentRunningStatus = mRunningStatus.getText().toString();
+////        int minOpt = 5;
+////        int midOpt =10;
+////        double maxOpt = 21.2;
+////
+////        int intTmpTarget = Integer.parseInt(target);
+////        double doubleTmpTarget = Double.parseDouble(target);
+////
+////        if (intTmpTarget != minOpt ||intTmpTarget != midOpt || doubleTmpTarget != maxOpt|| TextUtils.isEmpty(target)){
+////            Toast.makeText(this,"Please chose one from the options",Toast.LENGTH_SHORT).show();
+////            return;
+////        }
+////
+////        if (TextUtils.isEmpty(paceStatus) || TextUtils.isEmpty(currentRunningStatus)){
+////            Toast.makeText(this,"One of the fields is empty",Toast.LENGTH_SHORT).show();
+////            return;
+////        }
+////
+////
+////        // check the  range of current Running week range and status inputs.
+////        int minRun =0;
+////        int maxRun = 99;
+////        double minPace = 00.00;
+////        double maxPace = 99.55;
+////        int tmpRunStatus = Integer.parseInt(currentRunningStatus);
+////        double tmpPace = Double.parseDouble(paceStatus);
+////
+////        if (tmpRunStatus <= minRun || tmpRunStatus >= maxRun){
+////            Toast.makeText(this,"Please enter a valid running input",Toast.LENGTH_SHORT).show();
+////            return;
+////        }
+////        if (tmpPace <= minPace || tmpPace >= maxPace){
+////            Toast.makeText(this,"Please enter a valid pace input",Toast.LENGTH_SHORT).show();
+////            return;
+////        }
+//    }
